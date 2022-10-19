5deb4e5a21508dd65b1bbff7fa79550923daffff
BEN12342 <91931200+BEN12342@users.noreply.github.com>
2022-10-19 17:57:48 +1100

    Create Html.js

---
+++ Html.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script>
+    //This code accesses code from the file.
+    const div1 = document.getElementByClassName("div1")
+    div1.style.color = "green";
+    
+    </script>
+</head>
+<body>
+    <div class = "div1">Hello</div>
+</body>
+</html>
