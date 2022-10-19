18a2f2331d93bb98a4fcae090a17d4483edcee8d
BEN12342 <91931200+BEN12342@users.noreply.github.com>
2022-10-19 17:54:53 +1100

    Create Basics.js

---
+++ Basics.js
@@ -0,0 +1,41 @@
+console.log("Hello world!")
+
+//To run Javascript on your computer, download node and run in the terminal "node <filename>".
+
+//Javascript is usually ran on the browser, to help websites do things, such as move across the page, animation, APIs and more.
+
+//Variables:
+
+let a =1;
+//Function scoped, can be redeclared
+var b = 2;
+
+//Can't ever change this value
+const c = "Hello";
+
+let d = True;
+
+//If statements are mainly the same in python, however we add () around the condition and wrap {} around the code inside, eg:
+
+if (b > a) {
+  console.log("b is greater than a");
+} else if (a == b) {
+  console.log("a is b")
+} else {
+  console.log("a is greater than b");
+}
+//Same with while loops and for loops
+
+//One cool thing we can do with for loops is:
+let i;
+for (i = 0; i < 100; i++) {
+  console.log(i);
+}
+
+//In python this is for i in range(100):.
+
+
+//Arrays are the same, just use a keyword:
+
+let arr = ["hello", "goodbye"];
+
